ae31bebd4fa9773750cdf2a4f5653936de0e15d1
Benjamin Huang <bhuang@rms.com>
2019-08-03 16:55:45 -0700

    update

--- common/Shader.h
+++ common/Shader.h
@@ -1,23 +1,46 @@
-#ifndef SHADER_H
-#define SHADER_H
-#include <string>
+
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+//
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
 #include <GL/glew.h>
-#include "GLFW/glfw3.h"
+#include <string>
+#include "GameMath.h"
 
 class Shader
 {
 public:
-  Shader(const char *vertexPath, const char *fragemnetPath);
-  std::string vertexString;
-  std::string fragementString;
-  const char* vertexSource;
-  const char* fragementSource;
-  // Shader program ID
-  GLint ID;
-  void use();
-private:
-  void checkCompileErrors(GLint id);
-  void checkLinkErrors(GLint id);
-};
+	Shader();
+	~Shader();
+	// Load the vertex/fragment shaders with the given names
+	bool Load(const std::string &vertName, const std::string &fragName);
+	void Unload();
+	// Set this as the active shader program
+	void SetActive();
+	// Sets a Matrix uniform
+	void SetMatrixUniform(const char *name, const Matrix4 &matrix);
+	// Sets a Vector3 uniform
+	void SetVectorUniform(const char *name, const Vector3 &vector);
+	// Sets a float uniform
+	void SetFloatUniform(const char *name, float value);
 
-#endif
+private:
+	// Tries to compile the specified shader
+	bool CompileShader(const std::string &fileName, GLenum shaderType, GLuint &outShader);
+
+	// Tests whether shader compiled successfully
+	bool IsCompiled(GLuint shader);
+	// Tests whether vertex/fragment programs link
+	bool IsValidProgram();
+
+private:
+	// Store the shader object IDs
+	GLuint mVertexShader;
+	GLuint mFragShader;
+	GLuint mShaderProgram;
+};
--- common/shader.cpp
+++ common/shader.cpp
@@ -1,91 +1,150 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+//
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
 #include "Shader.h"
-
-#include <iostream>
-#include <sstream>
+#include <SDL.h>
 #include <fstream>
-#include <exception>
+#include <sstream>
 
-using namespace std;
-
-Shader::Shader(const char *vertexPath, const char *fragementPath)
+Shader::Shader(): mShaderProgram(0), mVertexShader(0), mFragShader(0)
 {
-	ifstream vertexFile;
-	ifstream fragementFile;
-	stringstream vertextSStream;
-	stringstream fragementSStream;
-
-	vertexFile.open(vertexPath);
-	fragementFile.open(fragementPath);
-
-	vertexFile.exceptions(ifstream::failbit | ifstream::badbit);
-	fragementFile.exceptions(ifstream::failbit | ifstream::badbit);
-
-	try
-	{
-		if (!vertexFile.is_open() || !fragementFile.is_open())
-			throw "Open file error";
-
-		vertextSStream << vertexFile.rdbuf();
-		fragementSStream << fragementFile.rdbuf();
-
-		vertexString = vertextSStream.str();
-		fragementString = fragementSStream.str();
-
-		vertexSource = vertexString.c_str();
-		fragementSource = fragementString.c_str();
-
-		GLint vertexId = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertexId, 1, &vertexSource, NULL);
-		glCompileShader(vertexId);
-		checkCompileErrors(vertexId);
-
-		GLint fragementId = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragementId, 1, &fragementSource, NULL);
-		glCompileShader(fragementId);
-		checkCompileErrors(fragementId);
-
-		ID = glCreateProgram();
-		glAttachShader(ID, vertexId);
-		glAttachShader(ID, fragementId);
-		glLinkProgram(ID);
-		checkLinkErrors(ID);
-
-		glDeleteShader(vertexId);
-		glDeleteShader(fragementId);
-	}
-	catch (const std::exception &ex)
-	{
-		cout << ex.what() << std::endl;
-	}
 }
 
-void Shader::use()
+Shader::~Shader()
 {
-	glUseProgram(ID);
 }
 
-void Shader::checkCompileErrors(GLint id)
+bool Shader::Load(const std::string &vertName, const std::string &fragName)
 {
-	int success;
-	char log[512];
-
-	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
-	if (!success)
+	// Compile vertex and pixel shaders
+	if (!CompileShader(vertName, GL_VERTEX_SHADER, mVertexShader) ||
+			!CompileShader(fragName, GL_FRAGMENT_SHADER, mFragShader))
 	{
-		glGetShaderInfoLog(id, 512, NULL, log);
-		cout << "shader compile error: " << log;
+		return false;
 	}
+
+	// Now create a shader program that
+	// links together the vertex/frag shaders
+	mShaderProgram = glCreateProgram();
+	glAttachShader(mShaderProgram, mVertexShader);
+	glAttachShader(mShaderProgram, mFragShader);
+	glLinkProgram(mShaderProgram);
+
+	// Verify that the program linked successfully
+	if (!IsValidProgram())
+	{
+		return false;
+	}
+
+	return true;
 }
 
-void Shader::checkLinkErrors(GLint id)
+void Shader::Unload()
 {
-	int success;
-	char log[512];
+	// Delete the program/shaders
+	glDeleteProgram(mShaderProgram);
+	glDeleteShader(mVertexShader);
+	glDeleteShader(mFragShader);
+}
 
-	glGetProgramiv(id, GL_COMPILE_STATUS, &success);
-	if (!success)
+void Shader::SetActive()
+{
+	// Set this program as the active one
+	glUseProgram(mShaderProgram);
+}
+
+void Shader::SetMatrixUniform(const char *name, const Matrix4 &matrix)
+{
+	// Find the uniform by this name
+	GLuint loc = glGetUniformLocation(mShaderProgram, name);
+	// Send the matrix data to the uniform
+	glUniformMatrix4fv(loc, 1, GL_TRUE, matrix.GetAsFloatPtr());
+}
+
+void Shader::SetVectorUniform(const char *name, const Vector3 &vector)
+{
+	GLuint loc = glGetUniformLocation(mShaderProgram, name);
+	// Send the vector data
+	glUniform3fv(loc, 1, vector.GetAsFloatPtr());
+}
+
+void Shader::SetFloatUniform(const char *name, float value)
+{
+	GLuint loc = glGetUniformLocation(mShaderProgram, name);
+	// Send the float data
+	glUniform1f(loc, value);
+}
+
+bool Shader::CompileShader(const std::string &fileName, GLenum shaderType, GLuint &outShader)
+{
+	// Open file
+	std::ifstream shaderFile(fileName);
+	if (shaderFile.is_open())
 	{
-		glGetProgramInfoLog(id, 512, NULL, log);
-		cout << "Program link error: " << log;
+		// Read all the text into a string
+		std::stringstream sstream;
+		sstream << shaderFile.rdbuf();
+		std::string contents = sstream.str();
+		const char *contentsChar = contents.c_str();
+
+		// Create a shader of the specified type
+		outShader = glCreateShader(shaderType);
+		// Set the source characters and try to compile
+		glShaderSource(outShader, 1, &(contentsChar), nullptr);
+		glCompileShader(outShader);
+
+		if (!IsCompiled(outShader))
+		{
+			SDL_Log("Failed to compile shader %s", fileName.c_str());
+			return false;
+		}
 	}
+	else
+	{
+		SDL_Log("Shader file not found: %s", fileName.c_str());
+		return false;
+	}
+
+	return true;
+}
+
+bool Shader::IsCompiled(GLuint shader)
+{
+	GLint status;
+	// Query the compile status
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
+	if (status != GL_TRUE)
+	{
+		char buffer[512];
+		memset(buffer, 0, 512);
+		glGetShaderInfoLog(shader, 511, nullptr, buffer);
+		SDL_Log("GLSL Compile Failed:\n%s", buffer);
+		return false;
+	}
+
+	return true;
+}
+
+bool Shader::IsValidProgram()
+{
+
+	GLint status;
+	// Query the link status
+	glGetProgramiv(mShaderProgram, GL_LINK_STATUS, &status);
+	if (status != GL_TRUE)
+	{
+		char buffer[512];
+		memset(buffer, 0, 512);
+		glGetProgramInfoLog(mShaderProgram, 511, nullptr, buffer);
+		SDL_Log("GLSL Link Status:\n%s", buffer);
+		return false;
+	}
+
+	return true;
 }
