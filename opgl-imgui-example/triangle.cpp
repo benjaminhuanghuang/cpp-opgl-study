73301b90b5a9a44757956bd3e6de3bb733f8d2b4
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-03-14 13:59:12 -0700

    triangle

--- opgl-imgui-example/triangle.cpp
+++ opgl-imgui-example/triangle.cpp
@@ -16,6 +16,11 @@ static void glfw_error_callback(int error, const char *description)
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f, // bottom left
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f};
+
 int main(int, char **)
 {
     // Setup window
@@ -60,42 +65,38 @@ int main(int, char **)
 
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    //初始化各种数据
-    GLuint VBO, VAO, EBO;
-    //创建一个VAO，并将它设为当前对象
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    //绑定顶点数组对象
-    glBindVertexArray(VertexArrayID);
+    //**********************************************************
+    // Prepare opengl drawing
+    //**********************************************************
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO); // create 1 vao
+    glBindVertexArray(VAO);
 
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,                 // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                 // size
+        GL_FLOAT,          // type
+        GL_FALSE,          // normalized?
+        3 * sizeof(float), // stride
+        (void *)0          // array buffer offset
+    );
+    glEnableVertexAttribArray(0);
+    // unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
     // 加载shader文件，创建并编译GLSL程序
     GLuint programID = LoadShaders("shaders/triangle.vert", "shaders/triangle.frag");
-    ImVec4 v1 = ImVec4(-0.25f, -0.25f, 0.0f, 1.00f);
-    ImVec4 v2 = ImVec4(0.25f, -0.25f, 0.0f, 1.00f);
-    ImVec4 v3 = ImVec4(0.0f, 0.25f, 0.0f, 1.00f);
-
-    //定义顶点缓冲，并将顶点缓冲传给OpenGL
-    GLuint vertexbuffer;
 
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-        GLfloat g_vertex_buffer_data[] = {
-            v1.x,
-            v1.y,
-            v1.z,
-            v2.x,
-            v2.y,
-            v2.z,
-            v3.x,
-            v3.y,
-            v3.z,
-        };
-        glGenBuffers(1, &vertexbuffer);
-        // 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
         // imgui
         {
             // Start the Dear ImGui frame
@@ -118,29 +119,13 @@ int main(int, char **)
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        //
-        // Draw Triangle
-
+        //********************************************
+        // Draw triangle
         glUseProgram(programID);
-
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        //设定顶点属性指针
-        glVertexAttribPointer(
-            0,        // attribute 0. No particular reason for 0, but must match the layout in the shader.
-            3,        // size
-            GL_FLOAT, // type
-            GL_FALSE, // normalized?
-            0,        // stride
-            (void *)0 // array buffer offset
-        );
-
-        // 画三角形
-        // glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-
-        // glDisableVertexAttribArray(0);
+        glBindVertexArray(VAO);
+        // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //********************************************
 
         //
         glfwSwapBuffers(window);
@@ -149,7 +134,6 @@ int main(int, char **)
     // 释放VAO、VBO、EBO资源
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     /*
      OpenGL Cleanup
         release ImGui resource
