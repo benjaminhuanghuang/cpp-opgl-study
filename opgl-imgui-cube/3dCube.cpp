6f7d8b04ea259a450ccb0fb5539868d152c99af6
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-03-17 17:20:05 -0700

    cube

--- opgl-imgui-cube/3dCube.cpp
+++ opgl-imgui-cube/3dCube.cpp
@@ -3,26 +3,59 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-#include "../common/VertexArray.h"
-#include "../common/Shader.h"
-#include "../common/Texture.h"
+#include "../common/shader-c.h"
+#define STB_IMAGE_IMPLEMENTATION
 
-float vertices[] = {
-    // Position, Normal, Texture coordinates
-    -0.5f, 0.5f, 0.0f, 0, 0, 0, 0.0f, 1.0f, // top left
-    0.5f, 0.5f, 0.0f, 0, 0, 0, 1.0f, 01.0f,  // top right
-    0.5f, -0.5f, 0.0f, 0, 0, 0, 1.0f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, 0, 0, 0, 0.0f, 0.0f // bottom left
-};
+#include "../common/stb_image.h"
 
-unsigned int indices[] = {
-    // the index starts from 0!
-    0, 1, 2, // 第一个三角形
-    2, 3, 0  // 第二个三角形
-};
+// use with Perspective Projection
+GLfloat vertices[] = {
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
 void processInput(GLFWwindow *window)
 {
@@ -40,7 +73,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(1280, 760, "Simple example", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -56,34 +89,124 @@ int main(void)
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
 
-    glViewport(0, 0, 1280, 760);
-    Shader shader;
-    shader.Load("shaders/cube.vert", "shaders/cube.frag");
+    glViewport(0, 0, 640, 480);
+    // Blend
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    //
+    glEnable(GL_DEPTH_TEST);
+    //**********************************************************
+    // Load shaders
+    //**********************************************************
+    GLuint programID = LoadShaders("shaders/cube.vert", "shaders/cube.frag");
 
-    VertexArray vao(vertices, 4, indices, 6);
+    //**********************************************************
+    // Load texture
+    //**********************************************************
+    int width, height, channels;
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    Texture texture;
-    texture.Load("images/wood.png");
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    unsigned char *image = stbi_load("images/wood.png", &width, &height, &channels, 0);
+    if (image == nullptr)
+    {
+        printf("SOIL failed to load image %s\n", "images/wood.png");
+    }
+
+    int format = GL_RGB;
+    if (channels == 4)
+    {
+        format = GL_RGBA;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    //**********************************************************
+    // Prepare opengl drawing
+    //**********************************************************
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); // create 1 vao
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+    // TexCoord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0); // Unbind VAO
+
+    glm::mat4 projection;
+    projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);
+    //projection =   glm::ortho(0.0f, ( GLfloat )screenWidth, 0.0f, ( GLfloat )screenHeight, 0.1f, 1000.0f);
+    
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         // Dark blue background
         glClearColor(0.0f, 0.0f, 0.4f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //-----------------------------------------------------------
+        // Activate shader
+        glUseProgram(programID);
 
-        // Draw
-        shader.SetActive();
-        vao.SetActive();
-        texture.SetActive();
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        //
+        // Using Texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(programID, "ourTexture"), 0);
+
+        //**********************************************************
+        // Transform
+        //**********************************************************
+        // Create transformations
+        glm::mat4 model(1.0f);
+        glm::mat4 view(1.0f);
+        model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f)); // use with perspective projection
+        //model = glm::rotate( model, 0.5f, glm::vec3( 1.0f, 0.0f, 0.0f ) ); // use to compare orthographic and perspective projection
+        //view = glm::translate( view, glm::vec3( screenWidth / 2, screenHeight / 2, -700.0f ) ); // use with orthographic projection
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // use with perspective projection
+
+        // Get their uniform location
+        GLint modelLoc = glGetUniformLocation(programID, "model");
+        GLint viewLoc = glGetUniformLocation(programID, "view");
+        GLint projLoc = glGetUniformLocation(programID, "projection");
+        // Pass them to the shaders
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        // Draw container
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        //----------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    //
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
